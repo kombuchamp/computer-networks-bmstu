461dc18322957f0f9552a565bbfa71bfb27d5b70
Konstantin Belousov <kombuchamp@gmail.com>
2020-05-10 14:58:23 +0300

    Add error handlers to node tcp server

--- Lab2_BasicHttpServer/server.js
+++ Lab2_BasicHttpServer/server.js
@@ -8,41 +8,43 @@ const path = require('path');
 
 // Create TCP socket, listen for connections
 net.createServer((socket) => {
-    socket.on('data', (buffer) => {
-        let req;
-        try {
-            req = parseRequest(buffer);
-            if (
-                req.method !== 'GET' || // Accept only get requests
-                !req.headers.Host || // Just check if its there as requested in lab description
-                !req.path.startsWith('/') ||
-                req.protocol !== 'HTTP/1.1'
-            ) {
-                throw new Error(`Parsed request is not supported: ${req.request}`);
+    socket
+        .on('data', (buffer) => {
+            let req;
+            try {
+                req = parseRequest(buffer);
+                if (
+                    req.method !== 'GET' || // Accept only get requests
+                    !req.headers.Host || // Just check if its there as requested in lab description
+                    !req.path.startsWith('/') ||
+                    req.protocol !== 'HTTP/1.1'
+                ) {
+                    throw new Error(`Parsed request is not supported: ${req.request}`);
+                }
+            } catch (e) {
+                console.error('Something went wrong', e);
+                socket.end(`HTTP/1.1 400 Bad Request${ENDL}${ENDL}`);
+                return;
             }
-        } catch (e) {
-            console.error('Something went wrong', e);
-            socket.end(`HTTP/1.1 400 Bad Request${ENDL}${ENDL}`);
-            return;
-        }
 
-        let file;
-        try {
-            let filePath = path.join(__dirname, req.path.slice(1) || 'index.html');
-            file = fs.readFileSync(filePath);
-            socket.write(`HTTP/1.1 200 OK${ENDL}`);
-            socket.write(
-                filePath.endsWith('.html')
-                    ? 'Content-Type: text/html; charset=UTF-8'
-                    : 'Content-Type: application/octet-stream' // Just send anything that is not html as a file
-            );
-            socket.write(`${ENDL}${ENDL}`);
-            socket.write(file);
-            socket.end(ENDL);
-        } catch {
-            socket.end(`HTTP/1.1 404 Not Found${ENDL}${ENDL}`);
-        }
-    });
+            let file;
+            try {
+                let filePath = path.join(__dirname, req.path.slice(1) || 'index.html');
+                file = fs.readFileSync(filePath);
+                socket.write(`HTTP/1.1 200 OK${ENDL}`);
+                socket.write(
+                    filePath.endsWith('.html')
+                        ? 'Content-Type: text/html; charset=UTF-8'
+                        : 'Content-Type: application/octet-stream' // Just send anything that is not html as a file
+                );
+                socket.write(`${ENDL}${ENDL}`);
+                socket.write(file);
+                socket.end(ENDL);
+            } catch {
+                socket.end(`HTTP/1.1 404 Not Found${ENDL}${ENDL}`);
+            }
+        })
+        .on('error', (error) => console.error('Got error: ', error));
 }).listen(PORT, () => console.log(`listening on port ${PORT}...`));
 
 function parseRequest(buffer) {
