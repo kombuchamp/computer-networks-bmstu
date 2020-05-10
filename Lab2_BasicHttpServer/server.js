223be74b1e0262839c0eecb45569af05642819cb
Konstantin Belousov <kombuchamp@gmail.com>
2020-05-10 14:29:32 +0300

    Refactor node tcp server

--- Lab2_BasicHttpServer/server.js
+++ Lab2_BasicHttpServer/server.js
@@ -1,5 +1,6 @@
 'use strict';
 const PORT = 3001;
+const ENDL = '\r\n';
 
 const net = require('net'); // TCP sockets node.js api
 const fs = require('fs');
@@ -11,19 +12,17 @@ net.createServer((socket) => {
         let req;
         try {
             req = parseRequest(buffer);
+            if (
+                req.method !== 'GET' || // Accept only get requests
+                !req.headers.Host || // Just check if its there as requested in lab description
+                !req.path.startsWith('/') ||
+                req.protocol !== 'HTTP/1.1'
+            ) {
+                throw new Error(`Parsed request is not supported: ${req.request}`);
+            }
         } catch (e) {
-            console.error('Something went wrong');
-            console.error(e);
-            socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
-            return;
-        }
-        if (
-            req.method !== 'GET' || // Accept only get requests
-            !req.headers.Host || // Just check if its there as requested in lab description
-            !req.path.startsWith('/') ||
-            req.protocol !== 'HTTP/1.1'
-        ) {
-            socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+            console.error('Something went wrong', e);
+            socket.end(`HTTP/1.1 400 Bad Request${ENDL}${ENDL}`);
             return;
         }
 
@@ -31,25 +30,24 @@ net.createServer((socket) => {
         try {
             let filePath = path.join(__dirname, req.path.slice(1) || 'index.html');
             file = fs.readFileSync(filePath);
-            socket.write('HTTP/1.1 200 OK\r\n');
+            socket.write(`HTTP/1.1 200 OK${ENDL}`);
             socket.write(
                 filePath.endsWith('.html')
                     ? 'Content-Type: text/html; charset=UTF-8'
-                    : 'Content-Type: application/octet-stream'
+                    : 'Content-Type: application/octet-stream' // Just send anything that is not html as a file
             );
-            socket.write('\r\n\r\n');
+            socket.write(`${ENDL}${ENDL}`);
             socket.write(file);
-            socket.end('\r\n');
-            return;
+            socket.end(ENDL);
         } catch {
-            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
-            return;
+            socket.end(`HTTP/1.1 404 Not Found${ENDL}${ENDL}`);
         }
     });
 }).listen(PORT, () => console.log(`listening on port ${PORT}...`));
 
 function parseRequest(buffer) {
-    let requestWithoutBody = buffer.toString().split('\r\n\r\n')[0].split('\r\n');
+    let request = buffer.toString();
+    let requestWithoutBody = request.split(`${ENDL}${ENDL}`)[0].split(ENDL);
     let startLine = requestWithoutBody.shift().split(' ');
     let headers = requestWithoutBody.reduce((result, line) => {
         let [, key, val] = line.match(/(.*): (.*)/);
@@ -57,6 +55,7 @@ function parseRequest(buffer) {
         return result;
     }, {});
     return {
+        request,
         method: startLine[0],
         path: startLine[1],
         protocol: startLine[2],
